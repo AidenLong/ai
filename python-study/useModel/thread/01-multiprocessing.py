e0e74400df965b259d048b6b15e9337827dc2a08
gaofan <807108578@qq.com>
2019-04-02 17:37:05 +0800

    多进程

--- python-study/useModel/thread/01-multiprocessing.py
+++ python-study/useModel/thread/01-multiprocessing.py
@@ -52,30 +52,30 @@ import multiprocessing
 
 
 #加锁
-# def work_1(f,n,lock):
-#     print('work_1 start')
-#     lock.acquire()
-#     for i in range(n):
-#         with open(f,'a') as fs:
-#             fs.write('i love pyhton \n')
-#             time.sleep(1)
-#     print('work_1 end')
-#     lock.release()
-#
-# def work_2(f,n,lock):
-#     print('work_2 start')
-#     lock.acquire()
-#     for i in range(n):
-#         with open(f,'a') as fs:
-#             fs.write('come on baby \n')
-#             time.sleep(1)
-#     print('work_2 end')
-#     lock.release()
-#
-# if __name__ == '__main__':
-#     lock=multiprocessing.Lock()
-#     p1 = multiprocessing.Process(target=work_1,args = ('file.txt',3,lock))
-#     p2 = multiprocessing.Process(target=work_2, args=('file.txt', 3,lock))
-#
-#     p1.start()
-#     p2.start()
+def work_1(f,n,lock):
+    print('work_1 start')
+    lock.acquire()
+    for i in range(n):
+        with open(f,'a') as fs:
+            fs.write('i love pyhton \n')
+            time.sleep(1)
+    print('work_1 end')
+    lock.release()
+
+def work_2(f,n,lock):
+    print('work_2 start')
+    lock.acquire()
+    for i in range(n):
+        with open(f,'a') as fs:
+            fs.write('come on baby \n')
+            time.sleep(1)
+    print('work_2 end')
+    lock.release()
+
+if __name__ == '__main__':
+    lock=multiprocessing.Lock()
+    p1 = multiprocessing.Process(target=work_1,args = ('file.txt',3,lock))
+    p2 = multiprocessing.Process(target=work_2, args=('file.txt', 3,lock))
+
+    p1.start()
+    p2.start()
